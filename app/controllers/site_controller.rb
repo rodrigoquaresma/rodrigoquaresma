eee9ebba4748b56b32ca089f4e1e33811101a56c
quaresma <rodrigoquaresma@gmail.com>
2014-06-29 16:10:38 -0300

    fix images path

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -32,7 +32,7 @@ class SiteController < ApplicationController
   end
 
   def facebook_gvt_tv
-    @area = 'facebook-gvt-tv'
+    @area = 'facebook_gvt_tv'
     @visu = 'tv'
     render :index
   end
@@ -44,13 +44,13 @@ class SiteController < ApplicationController
   end
 
   def guia_de_tv_tablet
-    @area = 'guia-de-tv-tablet'
+    @area = 'guia_de_tv_tablet'
     @visu = 'brands'
     render :index
   end
 
   def menu_de_apps
-    @area = 'menu-de-apps'
+    @area = 'menu_de_apps'
     @visu = 'display'
     render :index
   end
