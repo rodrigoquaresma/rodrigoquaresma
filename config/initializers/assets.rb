2fcd6ed203538828516569f4c3df869d0fe6c7a6
quaresma <rodrigoquaresma@gmail.com>
2014-07-01 23:10:52 -0300

    assets pre compile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,6 @@ Rails.application.config.assets.version = '1.0'
 base_css   = %w( site/styles-dark.css blenbox/blenbox.css )
 base_js    = %w( site/site.js blenbox/blenbox.js )
 vendor_css = %w( dragdealer.css )
-vendor_js  = %w( dragdealer.js )
+vendor_js  = %w( dragdealer.js jquery-1.9.1 ui/1.10.2/jquery-ui.js )
 
 Rails.application.config.assets.precompile += [ base_css, base_js, vendor_css, vendor_js ].flatten
