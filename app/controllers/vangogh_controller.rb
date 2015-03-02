822ef60d59459298c7535dadf026e20c97f134ab
rodrigo quaresma <rodrigoquaresma@quaresma.local>
2015-03-02 06:23:46 -0300

    in progress 3

--- app/controllers/vangogh_controller.rb
+++ app/controllers/vangogh_controller.rb
@@ -1,4 +1,16 @@
 class VangoghController < ApplicationController
   def index
   end
+  def experience
+  end
+  def about
+  end
+  def contact
+  end
+  def profile
+  end
+  def results
+  end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,67 +1,9 @@
 Rails.application.routes.draw do
-  # get 'vangogh/index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
 
 root 'site#index'
 
 get 'blenbox' => 'blenbox#index'
 
-get 'vangogh' => 'vangogh#index'
 
 get 'oxys' => 'oxys#index'
 get 'oxys/guide' => 'oxys#guide'
@@ -75,11 +17,12 @@ get 'instagvt' => 'site#instagvt'
 get 'guia-de-tv' => 'site#guia_de_tv'
 get 'menu-de-apps' => 'site#menu_de_apps'
 
+get 'vangogh' => 'vangogh#index'
+get 'vangogh/experience' => 'vangogh#experience'
+get 'vangogh/about' => 'vangogh#about'
+get 'vangogh/contact' => 'vangogh#contact'
+get 'vangogh/profile' => 'vangogh#profile'
 
-
-
-# get "/jobs" do
-#   erb :'rd/index'
-# end
-
+get 'vangogh/results' => 'vangogh#results'
+get 'vangogh/results/detail' => 'vangogh#show', as: 'vangogh_show'
 end
