bf6f43cb323fd63be0e5de72876f3ac7fda93d4d
rodrigo quaresma <rodrigoquaresma@quaresma.local>
2015-03-04 23:35:29 -0300

    progress

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 
 base_js    = %w( site/site.js blenbox/blenbox.js oxys/oxys.js oxys/guide.js vangogh.js )
-vendor_js  = %w( dragdealer.js jquery-1.9.1.js shortcut.js modernizr-2.8.3.js bootstrap3-typeahead.min.js )
+vendor_js  = %w( dragdealer.js jquery-1.9.1.js shortcut.js modernizr-2.8.3.js )
 base_css   = %w( site/styles-dark.css blenbox/blenbox.css oxys/oxys.css oxys/guide.css oxys/header.css oxys/mainmenu-geral.css oxys/mainmenu-covers.css vangogh.css )
 vendor_css = %w( dragdealer.css ui/1.10.2/themes/smoothness/jquery-ui.css )
 
