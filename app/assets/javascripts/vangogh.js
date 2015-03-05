25bdab17667a9dc5aee46b72446f7f3257c22f57
rodrigo quaresma <rodrigoquaresma@quaresma.local>
2015-03-05 14:24:37 -0300

    in progress - afu 2

--- app/assets/javascripts/vangogh.js
+++ app/assets/javascripts/vangogh.js
@@ -82,7 +82,7 @@
   	// the minimum number of items to show.
   	// when we resize the window, this will make sure minItems are always shown
   	// (unless of course minItems is higher than the total number of elements)
-  	minItems : 2,
+  	minItems : 4,
 
   	// index of the current item (left most item of the carousel)
   	start : 0
