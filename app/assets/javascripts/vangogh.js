571724ccb41d47a7663fe8a6ecde4b0ed85a2b4c
rodrigo quaresma <rodrigoquaresma@quaresma.local>
2015-03-05 13:22:36 -0300

    in progress - afu

--- app/assets/javascripts/vangogh.js
+++ app/assets/javascripts/vangogh.js
@@ -1,4 +1,104 @@
-(function ($){
+//= require modernizr.custom.17475
+//= require jquerypp.custom
+//= require jquery.elastislide
 
+(function ($){
+  // $.Elastislide.defaults = {
+  // 	// orientation 'horizontal' || 'vertical'
+  // 	orientation : 'horizontal',
+  //
+  // 	// sliding speed
+  // 	speed : 500,
+  //
+  // 	// sliding easing
+  // 	easing : 'ease-in-out',
+  //
+  // 	// the minimum number of items to show.
+  // 	// when we resize the window, this will make sure minItems are always shown
+  // 	// (unless of course minItems is higher than the total number of elements)
+  // 	minItems : 3,
+  //
+  // 	// index of the current item (left most item of the carousel)
+  // 	start : 0,
+  //
+  // 	// click item callback
+  // 	onClick : function( el, position, evt ) { return false; },
+  // 	onReady : function() { return false; },
+  // 	onBeforeSlide : function() { return false; },
+  // 	onAfterSlide : function() { return false; }
+  // };
+  //
+  // $('#carousel').elastislide();
+
+  // var = Elastislide.defaults = {
+  // 	// orientation 'horizontal' || 'vertical'
+  // 	orientation : 'horizontal',
+  //
+  // 	// sliding speed
+  // 	speed : 500,
+  //
+  // 	// sliding easing
+  // 	easing : 'ease-in-out',
+  //
+  // 	// the minimum number of items to show.
+  // 	// when we resize the window, this will make sure minItems are always shown
+  // 	// (unless of course minItems is higher than the total number of elements)
+  // 	minItems : 3,
+  //
+  // 	// index of the current item (left most item of the carousel)
+  // 	start : 0,
+  //
+  // 	// click item callback
+  // 	onClick : function( el, position, evt ) { return false; },
+  // 	onReady : function() { return false; },
+  // 	onBeforeSlide : function() { return false; },
+  // 	onAfterSlide : function() { return false; }
+  // };
+
+
+  var current = 0,
+	$preview = $( '#preview' ),
+	$carouselEl = $( '#carousel' ),
+	$carouselItems = $carouselEl.children(),
+	carousel = $carouselEl.elastislide( {
+		current : current,
+		// minItems : 4,
+		onClick : function( el, pos, evt ) {
+			changeImage( el, pos );
+			evt.preventDefault();
+		},
+		onReady : function() {
+			changeImage( $carouselItems.eq( current ), current );
+		},
+    // orientation 'horizontal' || 'vertical'
+  	orientation : 'horizontal',
+
+  	// sliding speed
+  	speed : 500,
+
+  	// sliding easing
+  	easing : 'ease-in-out',
+
+  	// the minimum number of items to show.
+  	// when we resize the window, this will make sure minItems are always shown
+  	// (unless of course minItems is higher than the total number of elements)
+  	minItems : 2,
+
+  	// index of the current item (left most item of the carousel)
+  	start : 0
+
+  	// click item callback
+  	// onClick : function( el, position, evt ) { return false; },
+  	// onReady : function() { return false; },
+  	// onBeforeSlide : function() { return false; },
+  	// onAfterSlide : function() { return false; }
+	});
+
+  function changeImage( el, pos ) {
+  	$preview.attr( 'src', el.data( 'preview' ) );
+  	$carouselItems.removeClass( 'current-img' );
+  	el.addClass( 'current-img' );
+  	carousel.setCurrent( pos );
+  }
 
 })(jQuery);
--- app/helpers/vangogh_helper.rb
+++ app/helpers/vangogh_helper.rb
@@ -6,8 +6,67 @@ module VangoghHelper
 
   def works
     [
-      [0,'Video On Demand - NETNOW','TV','On',['now01b.jpg','now01a.jpg']],
-      [1,'Facebook TV App','TV','Aplicativo para acesso do facebook pela TV.',['facebook-feed.jpg']]
+      [
+        0,
+        'Video On Demand - NETNOW',
+        'TV',
+        'Estava trabalhando no marketing e utilizando o design como ponte entre a área de produto e a engenharia no desenvolvimento de aplicações interativas para a TV, quando foi dado o pontapé inicial do projeto de serviço de vídeo sob demanda (VOD - video on demand) na NET, maior empresa de serviços de telecomunicações e entretenimento via cabo da América Latina. A minha sorte foi quando percebemos que as soluções de interface disponíveis no mercado não atendiam exatamente a expectativa que tínhamos. Era a deixa que eu precisava para demonstrar o potencial de trabalhar com foco no usuário e não mais na tecnologia.',
+        ['now01b.jpg','now01a.jpg','now02.jpg','now02a.jpg','now03.jpg','now04.jpg','now04a.jpg','now05.jpg','now06.jpg'],
+        [['interaction design',100],['graphic design',80]],
+        [1,2,3],
+        'great work! - wall street post',
+        ['//www.netcombo.com.br/now','product site','net now']
+      ],[
+        1,
+        'Facebook TV App',
+        'TV',
+        'Aplicativo para acesso do facebook pela TV.',
+        ['facebook-feed.jpg','facebook-albuns.jpg','facebook-foto-full.jpg'],
+        [['interaction design',100],['graphic design',80]],
+        [2,3,4],
+        'great work! - wall street post',
+        ['//www.gvt.com.br/PortalGVT/Residencial/TV-por-Assinatura#RedesSociais','product site','gvt tv facebook']
+      ],[
+        2,
+        'InstaGVT',
+        'TV',
+        'Aplicativo para acesso do instagram pela TV.',
+        ['instagvt-a.jpg','instagvt-b.jpg','instagvt-c.jpg'],
+        [['interaction design',100],['graphic design',80]],
+        [3,4,5],
+        'great work! - wall street post',
+        ['//www.gvt.com.br/PortalGVT/Residencial/TV-por-Assinatura#RedesSociais','product site','gvt tv instagram']
+      ],[
+        3,
+        'Guide TV',
+        'tablet',
+        'Guia de TV para tablet.',
+        ['guia-de-tv-tablet-a.jpg','guia-de-tv-tablet-b.jpg'],
+        [['interaction design',100],['graphic design',80]],
+        [4,5,0],
+        'great work! - wall street post',
+        ['//www.rodrigoquaresma.com.br/blenbox','demo/prototype','tv guide for tablet']
+      ],[
+        4,
+        'Apps menu for gvt tv',
+        'TV',
+        'Menu de aplicativos para set top box.',
+        ['menu-de-apps-a.jpg','menu-de-apps-b.jpg'],
+        [['interaction design',100],['graphic design',80]],
+        [5,0,1],
+        'great work! - wall street post',
+        ['//www.gvt.com.br/PortalGVT/Residencial/TV-por-Assinatura#RedesSociais','product site','gvt tv apps menu']
+      ],[
+        5,
+        'OxysTV',
+        'TV',
+        'OxysTV is a complete set of navigational services, including VoD and PPV services, PVR, content promotion and miniguide for linear TV.',
+        ['menu-de-apps-a.jpg','menu-de-apps-b.jpg'],
+        [['interaction design',100],['graphic design',80]],
+        [0,1,2],
+        'great work! - wall street post',
+        ['//www.rodrigoquaresma.com.br/oxys','demo/prototype','tv guide for tv']
+      ]
     ]
   end
 
